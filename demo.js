653fff1beee6acfb9762da972f34cd2db9257cf3
durgesh-developer <durgesh46it@gmail.com>
2022-06-16 12:10:46 +0530

    JS as a Fun

---
+++ demo.js
@@ -0,0 +1,23 @@
+let productNameInput = document.getElementById("product-name");
+let remainingChars = document.getElementById("remaining-chars");
+
+let maxAllowedChars = productNameInput.maxLength;
+
+function updateRemainingChars(event) {
+  let enteredText = event.target.value;
+  let enteredTextLength = enteredText.length;
+  // console.log(enteredTextLength);
+  let remainChar = maxAllowedChars - enteredTextLength;
+  // console.log(remainChar);
+  remainingChars.textContent = remainChar;
+}
+
+// function updateRemainingChars() {
+//   let enteredText = productNameInput.value;
+//   let enteredTextLength = enteredText.length;
+//   // console.log(enteredTextLength);
+//   let remainChar = maxAllowedChars - enteredTextLength;
+//   // console.log(remainChar);
+//   remainingChars.textContent = remainChar;
+// }
+productNameInput.addEventListener("input", updateRemainingChars);
